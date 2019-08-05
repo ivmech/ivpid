b80c5a0f6e6ca633fed837255d53302a9bf338d4
gpanouts <gpanoutsak@gmail.com>
2019-08-05 08:44:27 +0000

    Read and write frequency files

--- test_cache-sensitive.py
+++ test_cache-sensitive.py
@@ -1,89 +1,112 @@
 import PID
-import datetime
+import json
 import time
+import sys
+import signal
 import requests
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.interpolate import BSpline, make_interp_spline 
+
+
+def signal_handler(sig, frame):
+    for cpu in range(0,4):
+        fmax = open("/sys/devices/system/cpu/cpu" + str(cpu) + "/cpufreq/scaling_max_freq","w")
+        fmin = open("/sys/devices/system/cpu/cpu" + str(cpu) + "/cpufreq/scaling_min_freq","w")
+
+        fmin.write("1000000")
+        fmax.write("3700000")
+        fmin.close
+        fmax.close
+
+    sys.exit(0)
 
 def test_pid(P = 0.2,  I = 0.0, D= 0.0):
     """Self-test PID class
 
-    .. note::
-        ...
-        for i in range(1, END):
-            pid.update(feedback)
-            output = pid.output
-            if pid.SetPoint > 0:
-                feedback += (output - (1/i))
-            if i>9:
-                pid.SetPoint = 1
-            time.sleep(0.02)
-        ---
     """
-    r = requests.get('http://192.168.123.207:8001/v1/data')
+    print("The cache-sensitive app should run on cpus 0-3!!")
+    time.sleep(2)
+
+    r = requests.get('http://192.168.122.137:8001/v1/data')
     r.json()
-    print(r.text)
-    return
+    measurement = json.loads(r.text)
 
     pid = PID.PID(P, I, D)
 
-    pid.SetPoint=0.0
+    pid.SetPoint=0.9
     pid.setSampleTime(0.01)
 
-    feedback = 4.0
+    feedback = measurement["TranscodingMbps"]
 
     feedback_list = []
-    time_list = []
     setpoint_list = []
+    freqs = []
 
-    freqs = [ 1800000, 2800000, 3800000, 4800000, 5800000, 6800000, 7800000,]
-    pkt_loss = {
-        1800000: 2.3,
-        2800000: 2,
-        3800000: 1.8,
-        4800000: 1,
-        5800000: 0.5,
-        6800000: 0.5,
-        7800000: 0.5
-    }
+    f = 900000
+    for i in range(0, 28):
+        f += 100000
+        freqs.append(f)
 
     prev_idx = 0
+    freq = -1
+    signal.signal(signal.SIGINT, signal_handler)
+
     while True:
+        no_change = False
         pid.update(feedback)
         output = pid.output
         print("Output:" + str(output))
-        if output < 0:
-            if i == 1:
-                freq = freqs[0]
-                prev_idx = 0
-            elif prev_idx < len(freqs) - 1:
+
+        # Assumption: bigger frequency -> better performance
+        #TODO: find a smart way to convert pid output to frequency
+        if output > 0:
+            if prev_idx < len(freqs) - 1:
                 freq = freqs[prev_idx + 1]
                 prev_idx = prev_idx + 1
             else:
-                freq = freqs[prev_idx]
-        elif output > 0:
-            if i == 1:
-                freq = freqs[0]
-                prev_idx = 0
-            elif prev_idx > 0:
+                no_change = True
+        elif output < 0:
+            if prev_idx > 0:
                 freq = freqs[prev_idx - 1]
                 prev_idx = prev_idx - 1
             else:
-                freq = freqs[prev_idx]
+                if freq == freqs[0]:
+                    no_change = True
+                else:
+                    freq = freqs[0]
         else:
-            freq = freqs[prev_idx]
+            no_change = True
 
-        feedback = pkt_loss[freq]
-        time.sleep(0.02)
+
+        print("Frequency:" + str(freq))
+        if not no_change:
+            for cpu in range(0,4):
+                fmax = open("/sys/devices/system/cpu/cpu" + str(cpu) + "/cpufreq/scaling_max_freq","r+")
+                fmin = open("/sys/devices/system/cpu/cpu" + str(cpu) + "/cpufreq/scaling_min_freq","r+")
+
+                max_freq = int(fmax.read())
+                min_freq = int(fmin.read().strip())
+                fmax.seek(0)
+                fmin.seek(0)
+
+                if freq <= min_freq:
+                    fmin.write(str(freq))
+                    fmax.write(str(freq))
+                else:
+                    fmax.write(str(freq))
+                    fmin.write(str(freq))
+
+                fmax.close
+                fmin.close
+
+        time.sleep(1)
+        r = requests.get('http://192.168.122.137:8001/v1/data')
+        r.json()
+        measurement = json.loads(r.text)
+        feedback = measurement["TranscodingMbps"]
         print("Feedback:" + str(feedback))
 
         feedback_list.append(feedback)
         setpoint_list.append(pid.SetPoint)
-        time_list.append(i)
 
-    time_sm = np.array(time_list)
-    time_smooth = np.linspace(time_sm.min(), time_sm.max(), 300)
 
 
 if __name__ == "__main__":
